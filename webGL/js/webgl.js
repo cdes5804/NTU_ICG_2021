911980846f20db8839f0aabc628767e26644567f
TSAI MING HSUAN <joe85123@gmail.com>
2021-05-02 11:02:00 +0800

    add gouraud and phong shading

--- webGL/js/webgl.js
+++ webGL/js/webgl.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const vsSource = `
+const flatVsSource = `
     attribute vec4 aVertexPosition;
     attribute vec3 aVertexNormal;
     attribute vec3 aFrontColor;
@@ -9,32 +9,183 @@ const vsSource = `
     uniform mat4 uNormalMatrix;
     uniform mat4 uModelViewMatrix;
 
-	varying vec4 fragcolor;
-    varying highp vec3 vLighting;
+	varying vec4 vertexViewSpace;
+    varying vec3 vColor;
 
     void main(void) {
-        gl_Position = uProjectionMatrix * uModelViewMatrix * aVertexPosition;
-        fragcolor = vec4(aFrontColor.rgb, 1.0);
-
-        highp vec3 ambientLight = vec3(0.3, 0.3, 0.3);
-        highp vec3 directionalLightColor = vec3(1, 1, 1);
-        highp vec3 directionalVector = normalize(vec3(0.85, 0.8, 0.75));
-
-        highp vec4 transformedNormal = uNormalMatrix * vec4(aVertexNormal, 1.0);
-
-        highp float directional = max(dot(transformedNormal.xyz, directionalVector), 0.0);
-        vLighting = ambientLight + (directionalLightColor * directional);
+        vertexViewSpace = uModelViewMatrix * aVertexPosition;
+        gl_Position = uProjectionMatrix * vertexViewSpace;
+        vColor = aFrontColor;
     }
 `;
 
-const fsSource = `
+const flatFsSource = `
+    #extension GL_OES_standard_derivatives : enable
+    #define NUM_LIGHT 1
     precision mediump float;
 
-    varying vec4 fragcolor;
-    varying highp vec3 vLighting;
+    uniform float Ka;   // Ambient reflection coefficient
+    uniform float Kd;   // Diffuse reflection coefficient
+    uniform float Ks;   // Specular reflection coefficient
+    uniform float shininess;
+
+    varying vec4 vertexViewSpace;
+    varying vec3 vColor;
+    
+    uniform vec3 uLightSource[NUM_LIGHT];
+    uniform vec3 uLightColor[NUM_LIGHT];
+    
+    void main(void) {
+        vec3 U = dFdx(vertexViewSpace.xyz);                     
+        vec3 V = dFdy(vertexViewSpace.xyz);
+        vec3 N = normalize(cross(U, V));
+
+        vec4 color;
+        vec3 vertexPosition = vec3(vertexViewSpace) / vertexViewSpace.w;
+        
+        for (int i = 0; i < NUM_LIGHT; i++) {
+            vec3 L = normalize(uLightSource[i] - vertexPosition);
+            float lambertian = max(dot(N, L), 0.0);
+            float specular = 0.0;
+
+            if (lambertian > 0.0) {
+                vec3 R = reflect(-L, N);                  // Reflected light vector
+                vec3 V = normalize(-vertexPosition);      // Vector to viewer
+                // Compute the specular term
+                float specAngle = max(dot(R, V), 0.0);
+                specular = pow(specAngle, shininess);
+            }
+
+            color += vec4(Ka * vColor +
+                          Kd * lambertian * uLightColor[i] * vColor +
+                          Ks * specular * uLightColor[i] * vColor, 1.0);
+        }
+
+        gl_FragColor = color;
+    }
+`;
+
+const gouraudVsSource = `
+    #define NUM_LIGHT 1
+    attribute vec4 aVertexPosition;
+    attribute vec3 aVertexNormal;
+    attribute vec3 aFrontColor;
+
+    uniform mat4 uProjectionMatrix;
+    uniform mat4 uNormalMatrix;
+    uniform mat4 uModelViewMatrix;
+
+    varying vec4 vColor;
+
+    precision mediump float;
+
+    uniform float Ka;   // Ambient reflection coefficient
+    uniform float Kd;   // Diffuse reflection coefficient
+    uniform float Ks;   // Specular reflection coefficient
+    uniform float shininess;
+    
+    uniform vec3 uLightSource[NUM_LIGHT];
+    uniform vec3 uLightColor[NUM_LIGHT];
 
     void main(void) {
-        gl_FragColor = fragcolor;
+        vec4 vertexViewSpace = uModelViewMatrix * aVertexPosition;
+        gl_Position = uProjectionMatrix * vertexViewSpace;
+        vec3 vertexPosition = vec3(vertexViewSpace) / vertexViewSpace.w;
+        
+        vec3 normal = vec3(uNormalMatrix * vec4(aVertexNormal, 0.0));
+        vec3 N = normalize(normal);
+
+        for (int i = 0; i < NUM_LIGHT; i++) {
+            vec3 L = normalize(uLightSource[i] - vertexPosition);
+            float lambertian = max(dot(N, L), 0.0);
+            float specular = 0.0;
+            
+            if (lambertian > 0.0) {
+                vec3 R = reflect(-L, N);                  // Reflected light vector
+                vec3 V = normalize(-vertexPosition); // Vector to viewer
+                // Compute the specular term
+                float specAngle = max(dot(R, V), 0.0);
+                specular = pow(specAngle, shininess);
+            }
+
+            vColor += vec4(Ka * aFrontColor +
+                           Kd * lambertian * uLightColor[i] * aFrontColor +
+                           Ks * specular * uLightColor[i] * aFrontColor, 1.0);
+        }
+    }
+`;
+
+const gouraudFsSource = `
+    precision highp float;
+    varying vec4 vColor;
+
+    void main(void) {
+        gl_FragColor = vColor;
+    }
+`;
+
+const phongVsSource = `
+    attribute vec4 aVertexPosition;
+    attribute vec3 aVertexNormal;
+    attribute vec3 aFrontColor;
+
+    uniform mat4 uProjectionMatrix;
+    uniform mat4 uNormalMatrix;
+    uniform mat4 uModelViewMatrix;
+
+    varying vec3 vNormal;
+    varying vec3 vVertexPosition;
+    varying vec3 vColor;
+
+    void main(void) {
+        vec4 vertexViewSpace = uModelViewMatrix * aVertexPosition;
+        gl_Position = uProjectionMatrix * vertexViewSpace;
+
+        vVertexPosition = vec3(vertexViewSpace) / vertexViewSpace.w;
+        vNormal = vec3(uNormalMatrix * vec4(aVertexNormal, 0.0));
+        vColor = aFrontColor;
+}
+`;
+
+const phongFsSource = `
+    #define NUM_LIGHT 1
+    precision mediump float;
+
+    varying vec3 vNormal;
+    varying vec3 vVertexPosition;
+    varying vec3 vColor;
+
+    uniform float Ka;   // Ambient reflection coefficient
+    uniform float Kd;   // Diffuse reflection coefficient
+    uniform float Ks;   // Specular reflection coefficient
+    uniform float shininess;
+
+    uniform vec3 uLightSource[NUM_LIGHT];
+    uniform vec3 uLightColor[NUM_LIGHT];
+
+    void main(void) {
+        vec3 N = normalize(vNormal);
+        vec4 color;
+
+        for (int i = 0; i < NUM_LIGHT; i++) {
+            vec3 L = normalize(uLightSource[i] - vVertexPosition);
+            float lambertian = max(dot(N, L), 0.0);
+            float specular = 0.0;
+
+            if(lambertian > 0.0) {
+                vec3 R = reflect(-L, N);      // Reflected light vector
+                vec3 V = normalize(-vVertexPosition); // Vector to viewer
+                // Compute the specular term
+                float specAngle = max(dot(R, V), 0.0);
+                specular = pow(specAngle, shininess);
+            }
+
+            color += vec4(Ka * vColor +
+                          Kd * lambertian * uLightColor[i] * vColor +
+                          Ks * specular * uLightColor[i] * vColor, 1.0);
+        }
+
+        gl_FragColor = color;
     }
 `;
 
@@ -67,6 +218,7 @@ const initWebGL = (canvas) => {
     const gl = canvas.getContext('webgl') || canvas.getContext('experimental-webgl');
     gl.viewportWidth = canvas.width;
     gl.viewportHeight = canvas.height;
+    gl.getExtension('OES_standard_derivatives');
 
     return gl;
 }
@@ -85,7 +237,7 @@ const loadShader = (gl, type, source) => {
     return shader;
 }
 
-const initShader = (gl) => {
+const initShader = (gl, vsSource, fsSource) => {
     const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vsSource);
     const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fsSource);
     const shaderProgram = gl.createProgram();
@@ -146,7 +298,17 @@ const degToRad = (deg) => {
 }
 
 const drawObject = (gl, programInfo, objectInfo) => {
-    const { translate, rotation, scale, shear, buffers } = objectInfo;
+    const {
+        translate,
+        rotation,
+        scale,
+        shear,
+        directedLight,
+        Ka,
+        Kd,
+        Ks,
+        shininess,
+        buffers } = objectInfo;
     const { degree, rotationAxis } = rotation;
     const { lambda, shearAxis } = shear;
 
@@ -261,6 +423,27 @@ const drawObject = (gl, programInfo, objectInfo) => {
         false,
         modelViewMatrix);
     
+    const { source, color } = directedLight;
+    
+    gl.uniform3fv(
+        programInfo.uniformLocation.directedLight.source,
+        source);
+    gl.uniform3fv(
+        programInfo.uniformLocation.directedLight.color,
+        color);
+    gl.uniform1f(
+        programInfo.uniformLocation.Ka,
+        Ka);
+    gl.uniform1f(
+        programInfo.uniformLocation.Kd,
+        Kd);
+    gl.uniform1f(
+        programInfo.uniformLocation.Ks,
+        Ks);
+    gl.uniform1f(
+        programInfo.uniformLocation.shininess,
+        shininess);
+    
     {
         const offset = 0;
         const vertexCount = buffers.position.numItems;
@@ -291,11 +474,11 @@ const animate = (gl, programInfo, objectsInfo) => {
 }
 
 const main = async () => {
-    const objectsToDraw = ['Csie', 'Kangaroo'];
+    const objectsToDraw = ['Teapot'];
 
     const canvas = document.querySelector('#glcanvas');
     const gl = initWebGL(canvas);
-    const shaderProgram = initShader(gl);
+    const shaderProgram = initShader(gl, phongVsSource, phongFsSource);
 
     const programInfo = {
         program: shaderProgram,
@@ -308,6 +491,14 @@ const main = async () => {
             projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
             normalMatrix: gl.getUniformLocation(shaderProgram, 'uNormalMatrix'),
             modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+            directedLight: {
+                source: gl.getUniformLocation(shaderProgram, 'uLightSource'),
+                color: gl.getUniformLocation(shaderProgram, 'uLightColor'),
+            },
+            Ka: gl.getUniformLocation(shaderProgram, 'Ka'),
+            Kd: gl.getUniformLocation(shaderProgram, 'Kd'),
+            Ks: gl.getUniformLocation(shaderProgram, 'Ks'),
+            shininess: gl.getUniformLocation(shaderProgram, 'shininess'),
         },
     };
 
@@ -316,16 +507,24 @@ const main = async () => {
     for (const object of objectsToDraw) {
         const data = await getObject(object);
         const buffers = initBuffer(gl, data);
-        const translate = [0, 0, -10];
+        const translate = [0, 0, -50];
         const rotation = {
             degree: 0,
             rotationAxis: [0, 1, 0],
         };
         const scale = [1, 1, 1];
         const shear = {
-            lambda: [1, 1],
+            lambda: [0.1, 0.1],
             shearAxis: [0, 0, 0],
-        }
+        };
+        const directedLight = {
+            source: [0, 100, 0, 0, 0, 0, 0, 0, 0],
+            color: [0.1, 0.1, 0.1, 0.6, 0.6, 0.6, 0.6, 0.6, 0.6],
+        };
+        let Ka = 1.0;
+        let Kd = 1.0;
+        let Ks = 1.0;
+        let shininess = 80;
 
         objectInfo.push({
             buffers,
@@ -333,6 +532,11 @@ const main = async () => {
             rotation,
             scale,
             shear,
+            directedLight,
+            Ka,
+            Kd,
+            Ks,
+            shininess,
         });
     }
 
